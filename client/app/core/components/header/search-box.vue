e277ee7a98fe3a78fe0371de4cdbe79a7281f112
Hudson Kennedy <hudsonhugo@bol.com.br>
2021-12-07 15:45:04 -0300

    initialvue

---
+++ client/app/core/components/header/search-box.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+	.search-box
+		i.fa.fa-search
+		input(type="search", v-model="text", :placeholder="_('Search3dots')")
+
+</template>
+
+<script>
+	import { mapGetters, mapActions } from "vuex";
+
+	export default {
+		computed: {
+			...mapGetters("session", [
+				"searchText"
+			]),
+			text: {
+				get() {
+					return this.searchText;
+				},
+
+				set(value) {
+					this.searching(value);
+				}
+			}
+		},
+
+		methods: {
+			...mapActions("session", [
+				"searching"
+			])
+		}
+	};
+	
+</script>
+
+<style lang="scss">
+</style>
