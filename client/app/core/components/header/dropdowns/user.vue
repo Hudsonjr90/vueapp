b95f909582532435f96bf6656cd61001090fa788
Hudson Kennedy <hudsonhugo@bol.com.br>
2021-12-07 15:45:03 -0300

    initialvue

---
+++ client/app/core/components/header/dropdowns/user.vue
@@ -0,0 +1,39 @@
+<template lang="pug">
+	ul.dropdown-menu.user-menu(:class="{ 'visible': visible }")
+		router-link(tag="li", to="/profile")
+			a
+				.icon
+					i.fa.fa-user
+				| {{ "MyAccount" | i18n }}
+				
+		router-link(tag="li", to="/settings")
+			a
+				.icon
+					i.fa.fa-cog
+				| {{ "Settings" | i18n }}
+		
+		li.separator
+
+		li
+			a(href='/logout')
+				.icon
+					i.fa.fa-power-off
+				| {{ "Logout" | i18n }}
+
+
+</template>
+
+<script>
+
+	export default {
+
+		props: [
+			"visible"
+		]
+
+	};
+	
+</script>
+
+<style lang="scss">
+</style>
