d49edeb9ceaa0d6f7951aef4d3d8d41d974a0403
Hudson Kennedy <hudsonhugo@bol.com.br>
2021-12-07 15:45:05 -0300

    initialvue

---
+++ client/app/core/toastr.js
@@ -0,0 +1,10 @@
+import "toastr/build/toastr.css";
+import toastr from "toastr/build/toastr.min.js";
+
+toastr.options = {
+	debug: false,
+	newestOnTop: true,
+	timeOut: 5000
+};
+
+export default toastr;
