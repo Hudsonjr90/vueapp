2f532bcf2eab9568be78a2aaa1276846da5ce083
Hudson Kennedy <hudsonhugo@bol.com.br>
2021-12-07 15:45:09 -0300

    initialvue

---
+++ client/app/modules/session/store/getters.js
@@ -0,0 +1,15 @@
+export function me(state) {
+	return state.user;
+}
+
+export function notifications(state) {
+	return state.notifications;
+}
+
+export function messages(state) {
+	return state.messages;
+}
+
+export function searchText(state) {
+	return state.searchText;
+}
