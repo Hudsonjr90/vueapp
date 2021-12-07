7750f963a9a65950e5ded8ddbac75e34f0e921ac
Hudson Kennedy <hudsonhugo@bol.com.br>
2021-12-07 15:45:04 -0300

    initialvue

---
+++ client/app/core/components/header/logo.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+	.logo
+		a.nav-item(href="#")
+			span 
+				strong VEM
+				| App
+</template>
+
+<script>
+
+	export default {
+
+	};
+	
+</script>
+
+<style lang="scss">
+</style>
