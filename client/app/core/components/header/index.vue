1359a95993b456ea2ece6035f77a4f4ef9adc076
Hudson Kennedy <hudsonhugo@bol.com.br>
2021-12-07 15:45:04 -0300

    initialvue

---
+++ client/app/core/components/header/index.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+	section.page-header
+
+		logo.left
+
+		.menu-toggle.left(@click="toggleSidebar()")
+			i.fa.fa-bars
+
+		search-box.left
+
+		user-box.right
+
+</template>
+
+<script>
+	import Logo from "./logo";
+	import SearchBox from "./search-box";
+	import UserBox from "./user-box";
+
+	export default {
+		components: {
+			Logo,
+			SearchBox,
+			UserBox
+		},
+
+		props: [
+			"toggleSidebar"
+		]
+
+	};
+	
+</script>
+
+<style lang="scss">
+</style>
